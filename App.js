fea589c263ea14e795bdf989447b1def783b6ccc
Your Name <lokeshtiwari212@gmail.com>
2024-07-10 18:03:33 +0530

    used useColorScheme and some basic styling

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View,useColorScheme } from 'react-native'
 
 export default function App() {
+  const isDarkMode = useColorScheme() === 'dark'
   return (
-  <View style={{flex:1,borderWidth:2, borderColor:'red'}}>
-   
+  <View style={styles.container}>
+   <Text style={isDarkMode ? styles.whiteText : styles.darkText }>Hello World</Text>
   </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center'
+  },
+  whiteText:{
+    color:'#fff'
+  },
+  darkText:{
+    color:'#000'
+  }
+})

9b5e7acb0df455c58d5aef61f1dc3b810be3aa7d
Your Name <lokeshtiwari212@gmail.com>
2024-07-10 08:55:43 +0530

    new repo initialiazed

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+  <View style={{flex:1,borderWidth:2, borderColor:'red'}}>
+   
+  </View>
+  )
+}
